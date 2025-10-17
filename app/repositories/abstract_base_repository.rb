d9186f2e7496692c4fba2c97bedb188f0c59deae
Andoni Zabala <andonifzabala@gmail.com>
2025-10-17 10:27:49 -0300

    adds base repo

---
+++ app/repositories/abstract_base_repository.rb
@@ -0,0 +1,49 @@
+# typed: strict
+
+class AbstractBaseRepository
+  extend T::Sig
+  extend T::Helpers
+  extend T::Generic
+
+  abstract!
+
+  Entity = type_member { { upper: T::Struct } }
+
+  sig { returns(T.untyped) }
+  def self.entity
+    T.unsafe(const_get(:Entity)).inner_type[:fixed]
+  end
+
+  sig { overridable.params(dto: T.untyped).returns(T::Array[Entity]) }
+  def read(dto:)
+    raise_error(__method__)
+  end
+
+  sig { overridable.params(dto: IdDto).returns(T.nilable(Entity)) }
+  def find(dto:)
+    raise_error(__method__)
+  end
+
+  sig { overridable.params(dto: T.untyped).returns(Entity) }
+  def create(dto:)
+    raise_error(__method__)
+  end
+
+  sig { overridable.params(dto: T.untyped).returns(T.nilable(Entity)) }
+  def update(dto:)
+    raise_error(__method__)
+  end
+
+  sig { overridable.params(dto: IdDto).returns(T.nilable(Entity)) }
+  def destroy(dto:)
+    raise_error(__method__)
+  end
+
+  private
+
+  sig { params(method: T.nilable(Symbol)).returns(T.noreturn) }
+  def raise_error(method)
+    raise NotImplementedError,
+          "#{method} must be implemented in #{self.class} repository"
+  end
+end
