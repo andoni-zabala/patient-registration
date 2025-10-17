f143327cbcd28c43503d57705b7f7fa56fbeceb8
Andoni Zabala <andonifzabala@gmail.com>
2025-10-17 11:54:05 -0300

    adds patients controller

--- app/controllers/api/v1/patients_controller.rb
+++ app/controllers/api/v1/patients_controller.rb
@@ -1,2 +1,56 @@
-class Api::V1::PatientsController < ApplicationController
+# typed: strict
+
+module Api
+  module V1
+    class PatientsController < AbstractBaseController
+      extend T::Sig
+
+      sig { override.void }
+      def index
+        super
+      end
+
+      sig { override.void }
+      def show
+        super
+      end
+
+      sig { override.void }
+      def create
+        super
+      end
+
+      sig { override.void }
+      def update
+        super
+      end
+
+      sig { override.void }
+      def destroy
+        super
+      end
+
+      private
+
+      sig { override.returns(Patients::ReadDto) }
+      def read_dto
+        Patients::ReadDto.new
+      end
+
+      sig { override.returns(Patients::CreateDto) }
+      def create_dto
+        Patients::CreateDto.new
+      end
+
+      sig { override.returns(Patients::UpdateDto) }
+      def update_dto
+        Patients::UpdateDto.new
+      end
+
+      sig { override.returns(PatientsRepository) }
+      def repository
+        @repository ||= T.let(PatientsRepository.new, T.nilable(PatientsRepository))
+      end
+    end
+  end
 end
